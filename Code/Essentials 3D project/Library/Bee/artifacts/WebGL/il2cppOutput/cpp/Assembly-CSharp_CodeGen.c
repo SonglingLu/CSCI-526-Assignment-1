84e18fc525d8bb7e1d9e60b3fff87b28feabaad1
Songling Lu <sol104@ucsd.edu>
2023-09-01 01:29:37 -0700

    3D and Script

---
+++ Code/Essentials 3D project/Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,83 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void BallTransform_Start_m563D622B59DDCEEA517F3220294401EB82D3A7D1 (void);
+extern void BallTransform_Update_m441F920DDE6DAA94714A49576D9661E3C0E9B21F (void);
+extern void BallTransform__ctor_m482BF52575CBD675EB9D6D37085B645EE0573FE7 (void);
+extern void HelloWorld_Start_m7D4140BFC9DCC632B4B2907C1BFFAC4FF2FE9247 (void);
+extern void HelloWorld_Update_mD199B6A4EF418F7EBEE4AEC5645BB7B6215371C5 (void);
+extern void HelloWorld__ctor_m7E681D25064FF84A25730DDBC04091394DE55C35 (void);
+extern void PlatformRotate_Start_mEED1DF2EDEDA7DB41951FC1D699216312F145D7F (void);
+extern void PlatformRotate_Update_m19B12E0655C2AC0948DDD3A038EC7D0A29400827 (void);
+extern void PlatformRotate__ctor_mC82E71BD61D5DBC31CB7294FDCF1BCE3CFAA03A7 (void);
+extern void TrackPosition_Start_m42EB42686C302594A2F722963E649B754DDE9EED (void);
+extern void TrackPosition_Update_mD4EA07AF89642F0EB7BEBF7A4A59A9122AC71AD1 (void);
+extern void TrackPosition__ctor_mA94CC53524D5A65259C04792264571D11FE5C2C7 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1 (void);
+static Il2CppMethodPointer s_methodPointers[14] = 
+{
+	BallTransform_Start_m563D622B59DDCEEA517F3220294401EB82D3A7D1,
+	BallTransform_Update_m441F920DDE6DAA94714A49576D9661E3C0E9B21F,
+	BallTransform__ctor_m482BF52575CBD675EB9D6D37085B645EE0573FE7,
+	HelloWorld_Start_m7D4140BFC9DCC632B4B2907C1BFFAC4FF2FE9247,
+	HelloWorld_Update_mD199B6A4EF418F7EBEE4AEC5645BB7B6215371C5,
+	HelloWorld__ctor_m7E681D25064FF84A25730DDBC04091394DE55C35,
+	PlatformRotate_Start_mEED1DF2EDEDA7DB41951FC1D699216312F145D7F,
+	PlatformRotate_Update_m19B12E0655C2AC0948DDD3A038EC7D0A29400827,
+	PlatformRotate__ctor_mC82E71BD61D5DBC31CB7294FDCF1BCE3CFAA03A7,
+	TrackPosition_Start_m42EB42686C302594A2F722963E649B754DDE9EED,
+	TrackPosition_Update_mD4EA07AF89642F0EB7BEBF7A4A59A9122AC71AD1,
+	TrackPosition__ctor_mA94CC53524D5A65259C04792264571D11FE5C2C7,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1,
+};
+static const int32_t s_InvokerIndices[14] = 
+{
+	1099,
+	1099,
+	1099,
+	1099,
+	1099,
+	1099,
+	1099,
+	1099,
+	1099,
+	1099,
+	1099,
+	1099,
+	2126,
+	1099,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	14,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
